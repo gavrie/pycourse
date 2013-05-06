b15de2b90fa6293323f46e0304152e237a8ad1d1
Gavrie Philipson <gavriep@il.ibm.com>
2013-05-06 09:48:20 +0300

    fib3

--- examples/fib.py
+++ examples/fib.py
@@ -1,7 +1,10 @@
 # 1 1 2 3 5 8
 
+cache = {}
+
 def fib(n):
     if n < 2:
         return 1
-    return fib(n-1) + fib(n-2)
-
+    if n not in cache:
+        cache[n] = fib(n-2) + fib(n-1)
+    return cache[n]
