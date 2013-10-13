f0a1427bceba95a101ab2e6dc61167a3e1e9e48b
Gavrie Philipson <gavriep@il.ibm.com>
2013-10-13 15:11:57 +0300

    fizzbuzz

---
+++ examples/test_fizzbuzz.py
@@ -0,0 +1,23 @@
+"""
+FizzBuzz:
+
+Write a program that prints the numbers from 1 to 100.
+But for multiples of three print "Fizz" instead of the number
+and for the multiples of five print "Buzz".
+For numbers which are multiples of both three and five,
+print "FizzBuzz".
+"""
+
+from fizzbuzz import calc_fizzbuzz
+
+def test_fizzbuzz_number():
+    assert calc_fizzbuzz(2) == '2'
+
+def test_fizzbuzz_fizz():
+    assert calc_fizzbuzz(3) == 'Fizz'
+
+def test_fizzbuzz_buzz():
+    assert calc_fizzbuzz(5) == 'Buzz'
+
+def test_fizzbuzz_fizzbuzz():
+    assert calc_fizzbuzz(15) == 'FizzBuzz'
