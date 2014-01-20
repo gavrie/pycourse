5d655e0bcd8fbd42e5c0e09d7c12f50dccb38433
Gavrie Philipson <gavriep@il.ibm.com>
2014-01-20 11:01:15 +0200

    updated

--- exercises/test_fizzbuzz.py
+++ exercises/test_fizzbuzz.py
@@ -7,7 +7,10 @@ and for the multiples of five print "Buzz".
 For numbers which are multiples of both three and five,
 print "FizzBuzz".
 
+Online environment: http://repl.it/Nac/1
+
 To create your solution, you need to:
+
     1. Create a module named 'fizzbuzz.py'
     2. Write a function named 'calc_fizzbuzz' that works according to the tests.
     3. Run the test with the command 'py.test test_fizzbuzz.py'
@@ -18,13 +21,18 @@ To create your solution, you need to:
 from fizzbuzz import calc_fizzbuzz
 
 def test_fizzbuzz_number():
+    assert calc_fizzbuzz(1) == '1'
     assert calc_fizzbuzz(2) == '2'
+    assert calc_fizzbuzz(4) == '4'
 
 def test_fizzbuzz_fizz():
     assert calc_fizzbuzz(3) == 'Fizz'
+    assert calc_fizzbuzz(9) == 'Fizz'
 
 def test_fizzbuzz_buzz():
     assert calc_fizzbuzz(5) == 'Buzz'
+    assert calc_fizzbuzz(50) == 'Buzz'
 
 def test_fizzbuzz_fizzbuzz():
     assert calc_fizzbuzz(15) == 'FizzBuzz'
+    assert calc_fizzbuzz(45) == 'FizzBuzz'
