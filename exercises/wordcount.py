3143cb0a679dbc30758ae89de5df899d27d38b5d
Gavrie Philipson <gavriep@il.ibm.com>
2014-05-18 15:28:00 +0300

    wordcount

---
+++ exercises/wordcount.py
@@ -0,0 +1,11 @@
+"""
+Write a program that counts the count of characters, words and lines
+in its input, similar to the Unix "wc" command.
+
+Example output when run on the contents of this file:
+
+    $ python word_count.py test_word_count.py
+    29 lines
+    86 words
+    635 characters
+"""
