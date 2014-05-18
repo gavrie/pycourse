d087c1f97f287e31b2a476eee0c25ead4586f6ff
Gavrie Philipson <gavriep@il.ibm.com>
2014-05-18 15:53:13 +0300

    unique

---
+++ exercises/unique.py
@@ -0,0 +1,14 @@
+"""
+1. Write a function that determines if a string has all unique characters.
+2. What if you can not use additional data structures?
+"""
+
+def unique(s):
+    pass
+
+
+def test_unique():
+    assert unique("house")
+    assert not unique("direction")
+
+test_unique()
