bec9bfa4066985888a4c8f8b53953f31f21a0b10
Gavrie Philipson <gavriep@il.ibm.com>
2014-05-18 17:04:52 +0300

    passwd

--- examples/passwd.py
+++ examples/passwd.py
@@ -1,4 +1,15 @@
-with open("/etc/passwd") as f:
-    for line in f:
-        fields = line.strip().split(':')
-        print fields[0]
+import sys
+
+try:
+    filename = sys.argv[1]
+except IndexError:
+    print "Usage: passwd <filename>"
+    sys.exit(1)
+
+f = open(filename)
+
+for line in f:
+    # root:*:0:0:System Administrator:/var/root:/bin/sh
+    fields = line.strip().split(':')
+    username, _, _, _, fullname, _, _ = fields
+    print username, fullname
